5c1775e86807f554f08f0bb5f02b522567dedd3d
satkarjuneja <satkarjuneja@gmail.com>
2026-04-15 01:06:07 +0530

    Clean UP and Set up For Audio Features

--- engine.py
+++ engine.py
@@ -71,3 +71,45 @@ class MPVEngine:
 
     def set_pause(self, pause: bool):
         self._command(["set_property", "pause", "yes" if pause else "no"])
+  
+      
+import numpy as np        
+import subprocess
+import subprocess
+import numpy as np
+
+proc = subprocess.Popen(
+    ["yt-dlp", "-f", "bestaudio", "-o", "-",
+     "URL"],
+    stdout=subprocess.PIPE
+)
+
+ffmpeg = subprocess.Popen(
+    ["ffmpeg",
+     "-i", "pipe:0",
+     "-f", "f32le",
+     "-ac", "1",
+     "-ar", "48000",
+     "pipe:1"],
+    stdin=proc.stdout,
+    stdout=subprocess.PIPE
+)
+
+class Audio_Processing:
+    def __init__(self):
+        self.sample_rate = 48000
+        self.frame_size = 2048
+        self.hop_size = 1024
+        
+        self.window = np.hanning(self.frame_size)
+        self.buffer = np.zeros(self.frame_size * 4)
+        self.write_index = 0
+        self.spectrum = None
+        
+        self.bands = [
+            (20, 250),      # Bass
+            (250, 2000),    # Low-Mid
+            (2000, 6000),   # High-Mid
+            (6000, 16000)]   # Treble
+        
+        
